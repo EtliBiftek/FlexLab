4e644017171b86f2da673666923e56992e9745f0
Pifo <97687807+EtliBiftek@users.noreply.github.com>
2026-09-04 13:44:32 +0300

    Pass management token reliably to renderer

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,7 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+const tokenArg = process.argv.find((arg) => arg.startsWith('--flexlab-management-token='));
+const managementToken = tokenArg
+  ? tokenArg.slice('--flexlab-management-token='.length)
+  : (process.env.FLEXLAB_MANAGEMENT_TOKEN || '');
+
 contextBridge.exposeInMainWorld('flexlabDesktop', {
-  managementToken: process.env.FLEXLAB_MANAGEMENT_TOKEN || '',
+  managementToken,
   getInfo: () => ipcRenderer.invoke('desktop:get'),
   setOpenAtLogin: (enabled) => ipcRenderer.invoke('desktop:set-open-at-login', Boolean(enabled)),
   checkUpdates: () => ipcRenderer.invoke('desktop:check-updates'),
